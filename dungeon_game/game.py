3dc38e63ed3ab8734fba5461a0da4af6936952ff
hachtman <jackfllr1@googlemail.com>
2017-03-17 13:51:10 +0000

    Drawing out grid and full movement

--- dungeon_game/game.py
+++ dungeon_game/game.py
@@ -31,29 +31,24 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-
-def draw_map(grid_size):
+def create_map(grid_size):
     """Draws a map of selected size"""
-    game_grid = []  # Could be a tuple?
+    STATUS['game_grid'] = []  # Could be a tuple?
     STATUS['grid_size'] = grid_size
     x_coord = 1
     y_coord = 1
     grid_size_counter = grid_size * grid_size
     while (grid_size_counter):
-        game_grid.append([x_coord, y_coord])
+        STATUS['game_grid'].append([x_coord, y_coord])
         x_coord += 1
         if (x_coord == grid_size + 1):
             y_coord += 1
             x_coord = 1
         grid_size_counter -= 1
-    print(game_grid)
 
-
-draw_map(5)
 
 def set_locations():
     """Set the locations of the weapon and the monster"""
-
 
 
 def get_locations():
@@ -69,13 +64,13 @@ def get_moves():
     # If player's x coord is 0, they can't move left
     # If player's x coord is grid size, they can't move right
     if (x_coord == 1 and y_coord == 1):
-        return ['RIGHT', 'DOWN']
-    elif (x_coord == STATUS['grid_size'] and y_coord == STATUS['grid_size']):
-        return ['UP', 'LEFT']
-    elif (x_coord == 1 and y_coord == STATUS['grid_size']):
         return ['UP', 'RIGHT']
-    elif (x_coord == STATUS['grid_size'] and y_coord == 1):
+    elif (x_coord == STATUS['grid_size'] and y_coord == STATUS['grid_size']):
+        return ['DOWN', 'LEFT']
+    elif (x_coord == 1 and y_coord == STATUS['grid_size']):
         return ['DOWN', 'RIGHT']
+    elif (x_coord == STATUS['grid_size'] and y_coord == 1):
+        return ['UP', 'LEFT']
     elif (x_coord == 1):
         return ['UP', 'RIGHT', 'DOWN']
     elif (y_coord == 1):
@@ -99,24 +94,65 @@ def move_player(move):
     elif (move == 'LEFT'):
         STATUS['locations']['player'][0] -= 1
     else:
-        STATUS['locations']['player'][1] += 1
+        STATUS['locations']['player'][0] += 1
     print(STATUS['locations']['player'])
 
 
-def options():
-    print("Welcome to the dungeon!\n*************************")
-    print("Find the sword to kill the monster, then escape the dungeon.")
+def get_dungeon_size():
+    size = input("Choose the size of the dungeon... (4 - 24)\n>")
+    size = int(size)
+    while (size < 4 or size > 24):
+        print("Pick a number between four and 24.")
+        size = input("Choose the size of the dungeon... (4 - 24)")
+    return size
+
+
+def draw_dungeon():
+    grid_size = STATUS['grid_size']
+    cells = STATUS['game_grid']
+    player = get_locations()['player']
+    print(" _" * grid_size)
+    tile = "|{}"
+    for cell in cells:
+        x, y = cell
+        if x < grid_size:
+            line_end = ''
+            if cell == player:
+                output = tile.format("P")
+            else:
+                output = tile.format("_")
+        else:
+            line_end = "\n"
+            if cell == player:
+                output = tile.format("P|")
+            else:
+                output = tile.format("_|")
+        print(output, end=line_end)
+
+
+def run_game():
     while True:
         valid_moves = get_moves()
         print("You're currently in room {}.".format(STATUS['locations']['player']))
         print("You can move {}".format(valid_moves))
-
         move = input("> ").upper()
-        if move not in valid_moves:
-            print("Please try again: ")
+        while move not in valid_moves:
+            clear_screen()
+            draw_dungeon()
+            print("There's no door that way... ")
             move = input("> ").upper()
         else:
+            clear_screen()
             move_player(move)
+            draw_dungeon()
 
 
-options()
+def init():
+    clear_screen()
+    print("Welcome to the dungeon!\n*************************")
+    print("Find the sword to kill the monster, then escape the dungeon.")
+    create_map(get_dungeon_size())
+    run_game()
+
+
+init()
