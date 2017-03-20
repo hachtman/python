53cc18f2e420b69afc511019eddcad03fc1eb9dd
hachtman <jackfllr1@googlemail.com>
2017-03-20 14:39:30 +0000

    wrote monster, player and combat classes. learning python inheritance

--- oop/monster.py
+++ oop/monster.py
@@ -1,9 +1,57 @@
-class Monster:
+"""Monster Class"""
+import random
+
+from combat import Combat
+
+
+COLORS = ["Yellow", "Red", "Pink", "Green"]
+
+
+class Monster(Combat):
+    min_hit_points = 1
+    max_hit_points = 1
+    min_experience = 1
+    max_experience = 1
+    weapon = "claws"
+    sound = "snicker"
+
     def __init__(self, **kwargs):
-        self.hit_points = kwargs.get('hit_points', 1)
-        self.weapon = kwargs.get('weapon', 1)
-        self.color = kwargs.get('color', 1)
-        self.sound = kwargs.get('sound', 1)
+        self.hit_points = random.randint(self.min_hit_points, self.max_hit_points)
+        self.experience = random.randint(self.min_experience, self.max_experience)
+        self.color = random.choice(COLORS)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+    def __str__(self):
+        print('firing')
+        return '{} {}, HP: {}, XP: {}.'.format(self.color.title(),
+                                               self.__class__.__name__,
+                                               self.hit_points,
+                                               self.experience)
 
     def battlecry(self):
+        """returns monster's sound"""
         return self.sound.upper()
+
+
+
+class Goblin(Monster):
+    max_hit_points = 3
+    max_experience = 4
+    sound = 'sneer'
+
+class Troll(Monster):
+    min_hit_points = 5
+    max_hit_points = 50
+    min_experience = 2
+    max_experience = 3
+    sound = 'hurrumph'
+
+class Dragon(Monster):
+    min_hit_points = 2
+    max_hit_points = 5000
+    min_experience = 1
+    max_experience = 100
+    sound = 'Roar'
