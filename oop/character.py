5cf38a6257a65d666a09d0baac27c674bd9c4276
hachtman <jackfllr1@googlemail.com>
2017-03-20 14:42:38 +0000

    added base hp and rest method to character class

--- oop/character.py
+++ oop/character.py
@@ -30,6 +30,14 @@ class Character(Combat):
     def __init__(self, **kwargs):
         self.name = input("Name: ")
         self.weapon = self.get_weapon()
+        self.hit_points = self.base_hit_points
 
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    def __str__(self):
+        return '{}, HP: {}, XP: {}'.format(self.name, self.hit_points, self.experience)
+
+    def rest(self):
+        if self.hit_points < self.base_hit_points:
+            self.hit_points += 1
