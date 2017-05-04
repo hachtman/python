775f7f1c63689ddf9d92770a468666de0c5bcff9
hachtman <jackfllr1@googlemail.com>
2017-05-04 18:48:51 +0100

    added snippetDetail class

--- drf/tutorial/snippets/views.py
+++ drf/tutorial/snippets/views.py
@@ -2,13 +2,14 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from snippets.models import Snippet
+from rest_framework.views import APIView
+from django.http import Http404
 from snippets.serializers import SnippetSerializer
 # Create your views here.
 
-
-class Snippet(APIView):
+class SnippetList(APIView):
     """
-    List all Snippets, or create a new snippet.
+    List all snippets, or create a new snippet.
     """
     def get(self, request, format=None):
         snippets = Snippet.objects.all()
@@ -20,4 +21,33 @@ class Snippet(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_Bad_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
