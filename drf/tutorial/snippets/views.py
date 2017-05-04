8a1313ddf1a072ca9bfca0c7c22510a78465cbf7
hachtman <jackfllr1@googlemail.com>
2017-05-04 11:51:01 +0100

    refactored snippet view to be class based

--- drf/tutorial/snippets/views.py
+++ drf/tutorial/snippets/views.py
@@ -6,45 +6,18 @@ from snippets.serializers import SnippetSerializer
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+class Snippet(APIView):
     """
-    List all code snippets or create a new snippet
+    List all Snippets, or create a new snippet.
     """
-
-    if request.method == 'GET':
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
-    """
-    Retrieve update or delete a code snippet.
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_Bad_REQUEST)
