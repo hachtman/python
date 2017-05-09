0080ae616c09a3f1a3764d1c5ad1b1ec0aa95ff8
hachtman <jackfllr1@googlemail.com>
2017-05-09 15:47:32 +0100

    Flask forms skeleton

---
+++ flask/forms/forms/forms/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/save', methods=['POST'])
+def save():
+    import pdb
+    pdb.set_trace()
+    return redirect(url_for('index'))
+
+
+app.run(debug=True)
